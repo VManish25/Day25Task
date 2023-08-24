ab9524b796f097180a76170197dee74b0be7eeba
manish <manish25.mv@gmail.com>
2023-08-24 19:27:20 +0530

    SB ADMIN

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Sidebar from "./component/Sidebar";
+import Dashboard from "./component/Dashboard";
+
+function App() {
+  return <>
+<div id="wrapper"></div>
+  <Sidebar/>
+  <Dashboard/>
+  </>
+}
+
+export default App;
